209b6b1d9738c124930330c673a662f0bd3d886f
davidwagura <davidwagura83@gmail.com>
2023-03-03 12:22:02 +0300

    I added JavaScript to my HTML file

---
+++ javascript.js
@@ -0,0 +1,20 @@
+
+  const changeTitle = (text) => {
+   	document.getElementById('title').innerHTML = text
+  }
+	const myFunction  = (text) => {
+   	document.getElementById('title').innerHTML = "Agriculture";
+  }
+	const displayText = (text) => {
+	  document.getElementById('title').innerHTML = "Education";
+	}     
+  const showText = (text) => {
+	  document.getElementById('title').innerHTML = "Roads";    
+	}
+  function getRandomItem(arr) {
+  const item = arr[randomIndex];
+  return item;
+  }
+
+  const array = ['Davie', 'Alex', 'Christine', 'Joseph', 'Joan', 'Simon', 'Moses'];
+  
